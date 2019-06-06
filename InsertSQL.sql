b6ac9e16e2ec3ee47d80374f903b6735e42e2142
anjansharmasid <anjansharmasid@gmail.com>
2019-06-06 13:52:20 +0100

    ID added
    ID added

--- InsertSQL.sql
+++ InsertSQL.sql
@@ -1,23 +1,23 @@
 
 	
-insert into Sport_Events(eventName,dateOfEvent,description,eventType,venue,importantInfo,roomTypes,eventImage) 
+insert into Sport_Events(ID,eventName,dateOfEvent,description,eventType,venue,importantInfo,roomTypes,eventImage) 
 values 
-    ('Football Premier League Chelsea FC',    '2020-10-02 02:00:00',     'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Premier League Everton ',    '2020-10-03 02:00:00',       'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL' ),
-    ('Football Premier League Liverpool FC',  '2020-10-04 02:00:00',     'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL' ),
-    ('Football Premier League Manchester City', '2020-10-05 02:00:00',   'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Premier League Manchester United', '2020-10-06 02:00:00', 'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Premier League Newcastle United', '2020-10-07 02:00:00',  'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Premier League Southampton FC', '2020-10-08 02:00:00',    'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Premier League Tottenham Hotspur', '2020-10-09 02:00:00', 'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Premier League Wolverhampton Wanderers', '2020-10-10 02:00:00', 'Football Premier League', 'Premier League', 'Test Vanue',  'importantInfo', 'test', 'someURL'),
-    ('Football Serie A Inter Milan',                    '2020-10-01 11:00:00', 'Football Serie A',  'Serie A' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Atletico Madrid',                '2020-10-12 02:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Barcelona FC',                   '2020-10-01 14:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Espanyol',                       '2020-10-15 02:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Girona FC',                      '2020-10-16 02:00:00', 'Football La Liga ','La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Real Madrid',                    '2020-10-17 02:00:00', 'Football La Liga ','La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Sevilla',                        '2020-10-18 02:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football La Liga Valencia',                       '2020-10-19 02:00:00',  'Football La Liga',  'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football Primeira Liga SL Benfica',               '2020-10-20 02:00:00', 'Football La ','Liga La Liga ', 'Test Vanue', 'importantInfo', 'test', 'someURL'),
-    ('Football Primeira LigaSporting CP',               '2020-10-21 02:00:00', 'Football Primeira Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL');
+    (1,'Football Premier League Chelsea FC',    '2020-10-02 02:00:00',     'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (2,'Football Premier League Everton ',    '2020-10-03 02:00:00',       'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL' ),
+    (3,'Football Premier League Liverpool FC',  '2020-10-04 02:00:00',     'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL' ),
+    (4,'Football Premier League Manchester City', '2020-10-05 02:00:00',   'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (5,'Football Premier League Manchester United', '2020-10-06 02:00:00', 'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (6,'Football Premier League Newcastle United', '2020-10-07 02:00:00',  'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (7,'Football Premier League Southampton FC', '2020-10-08 02:00:00',    'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (8,'Football Premier League Tottenham Hotspur', '2020-10-09 02:00:00', 'Football Premier League',  'Premier League',  'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (9,'Football Premier League Wolverhampton Wanderers', '2020-10-10 02:00:00', 'Football Premier League', 'Premier League', 'Test Vanue',  'importantInfo', 'test', 'someURL'),
+    (10,'Football Serie A Inter Milan',                    '2020-10-01 11:00:00', 'Football Serie A',  'Serie A' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (11,'Football La Liga Atletico Madrid',                '2020-10-12 02:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (12,'Football La Liga Barcelona FC',                   '2020-10-01 14:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (13,'Football La Liga Espanyol',                       '2020-10-15 02:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (14,'Football La Liga Girona FC',                      '2020-10-16 02:00:00', 'Football La Liga ','La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (15,'Football La Liga Real Madrid',                    '2020-10-17 02:00:00', 'Football La Liga ','La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (16,'Football La Liga Sevilla',                        '2020-10-18 02:00:00', 'Football La Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (17,'Football La Liga Valencia',                       '2020-10-19 02:00:00',  'Football La Liga',  'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (18,'Football Primeira Liga SL Benfica',               '2020-10-20 02:00:00', 'Football La ','Liga La Liga ', 'Test Vanue', 'importantInfo', 'test', 'someURL'),
+    (19,'Football Primeira LigaSporting CP',               '2020-10-21 02:00:00', 'Football Primeira Liga', 'La Liga' , 'Test Vanue', 'importantInfo', 'test', 'someURL');
